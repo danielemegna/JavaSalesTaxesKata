a0f1066f93812c3c53883576572c1c40839d9166
Daniele Megna <megna.dany@gmail.com>
2015-08-04 11:05:22 +0200

    Little formatting refactoring in test

--- tests/CashDeskTest.java
+++ tests/CashDeskTest.java
@@ -9,7 +9,10 @@ public class CashDeskTest {
         CashDesk cs = new CashDesk();
         cs.scanProduct("1 book at 12.49");
 
-        String expected = "1 book: 12.49 Sales Taxes: 0.00 Total: 12.49";
+        String expected =
+            "1 book: 12.49" + " " +
+            "Sales Taxes: 0.00" + " " +
+            "Total: 12.49";
         String receipt = cs.produceReceipt();
 
         assertEquals(expected, receipt);
