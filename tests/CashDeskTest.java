9ace8cf0eaff7290027afd3d680335babe0ea0c3
Daniele Megna <megna.dany@gmail.com>
2015-08-04 11:19:46 +0200

    Removing duplication from tests

--- tests/CashDeskTest.java
+++ tests/CashDeskTest.java
@@ -1,34 +1,43 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class CashDeskTest {
 
+    private CashDesk cashDesk;
+
+    @Before
+    public void setup() {
+        cashDesk = new CashDesk();
+    }
+
     @Test
     public void sellingOneBook() {
-        CashDesk cs = new CashDesk();
-        cs.scanProduct("1 book at 12.49");
+        cashDesk.scanProduct("1 book at 12.49");
 
         String expected =
             "1 book: 12.49" + " " +
             "Sales Taxes: 0.00" + " " +
             "Total: 12.49";
-        String receipt = cs.produceReceipt();
 
-        assertEquals(expected, receipt);
+        assertReceipt(expected);
     }
 
     @Test
     public void sellingOneChocolateBar() {
-        CashDesk cs = new CashDesk();
-        cs.scanProduct("1 chocolate bar at 0.85");
+        cashDesk.scanProduct("1 chocolate bar at 0.85");
 
         String expected =
             "1 chocolate bar: 0.85" + " " +
             "Sales Taxes: 0.00" + " " +
             "Total: 0.85";
-        String receipt = cs.produceReceipt();
 
+        assertReceipt(expected);
+    }
+
+    private void assertReceipt(String expected) {
+        String receipt = cashDesk.produceReceipt();
         assertEquals(expected, receipt);
     }
 }
