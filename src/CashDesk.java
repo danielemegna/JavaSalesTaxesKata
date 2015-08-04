7b3d1266ae16bf6dfc6296fe724b8a5a16f5ccef
Daniele Megna <megna.dany@gmail.com>
2015-08-04 11:04:32 +0200

    Shortest pass of refactoring

--- src/CashDesk.java
+++ src/CashDesk.java
@@ -1,13 +1,23 @@
 public class CashDesk {
+    private String product;
+
     public static CashDesk build(String products) {
         return null;
     }
 
     public String produceReceipt() {
-        return "1 book: 12.49 Sales Taxes: 0.00 Total: 12.49";
+        String[] splittedProduct = product.split(" ");
+        String quantity = splittedProduct[0];
+        String name = splittedProduct[1];
+        String price = splittedProduct[3];
+
+        return
+            quantity + " " + name + ": " + price + " " +
+            "Sales Taxes: 0.00" + " " +
+            "Total: " + price;
     }
 
     public void scanProduct(String product) {
-
+        this.product = product;
     }
 }
